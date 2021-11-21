d49b1112bcf14a0f3b054813a7afbe0fa721acf6
lucaspere <lucasrupestre@hotmail.com>
2021-11-21 07:59:17 -0300

    conserta usuarios router

--- Código/server/src/routes/index.ts
+++ Código/server/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import empresaRouter from './empresa';
 import usuarioRouter from './usuario';
+import alunoRouter from './aluno';
 const router = Router();
 
 router.use('/empresas', empresaRouter);
-router.use('/usuario', empresaRouter);
+router.use('/usuarios', usuarioRouter);
+router.use('/alunos', alunoRouter);
 
 export default router;
