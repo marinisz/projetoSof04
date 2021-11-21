bdb1bbc7e76cbf7cf15cd78f6db47aaae055ffd6
lucaspere <lucasrupestre@hotmail.com>
2021-11-21 02:41:01 -0300

    remove arquivos desnecessarios

--- Código/server/src/interfaces/AbstractStore.ts
+++
@@ -1,8 +0,0 @@
-export interface Repository<T> {
-    update: (model: T) => Promise<T>;
-    create: (model: T) => Promise<T>;
-    read: (model: T) => Promise<T>;
-    delete: (model: T) => Promise<void>;
-    keylist(): (model: T) => Promise<string[]>;
-    count(): (model: T) => Promise<number>;
-}
