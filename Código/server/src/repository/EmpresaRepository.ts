8f1907328d9867f9105b9d9df9dd0c69bbfa8f6a
lucaspere <lucasrupestre@hotmail.com>
2021-11-21 02:26:32 -0300

    Cria EmpresaRepository

--- Código/server/src/repository/EmpresaRepository.ts
+++ Código/server/src/repository/EmpresaRepository.ts
@@ -1,5 +1,23 @@
+import fsDB from '../db/fs-acess'
 
-const empresas: Empresa[] = []
 class EmpresaRepository {
+    async cadastrarVantagem(key: string, data: any) {
+        const result = fsDB.save(data);
 
+        return result;
+    }
+
+    async deletarVantagem(query: { cnpj: string; vid: string }, data: any) {
+        const result = fsDB.save(data);
+
+        return result;
+    }
+
+    async alterarVantagem(key: any) {
+        const result = fsDB.save(key);
+
+        return result;
+    }
 }
+
+export default new EmpresaRepository();
