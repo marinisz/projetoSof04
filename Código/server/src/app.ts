4347139af5b16360df0fee5841dad33be2b1d901
lucaspere <lucasrupestre@hotmail.com>
2021-11-21 00:49:29 -0300

    adiciona basicErrorHandler

--- Código/server/src/app.ts
+++ Código/server/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import router from './routes';
+import { basicErrorHandler } from './appsupport';
 
 const PORT = process.env.PORT || 3000
-
 const app = express();
 
+app.use(express.json())
 app.use('/api/', router)
 
+app.use(basicErrorHandler)
 app.listen(PORT, () => { console.log('Serve listening on port', PORT)});
