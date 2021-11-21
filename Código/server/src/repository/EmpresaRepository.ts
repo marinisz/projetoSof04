73e9175eeb5c7d69ea416ffef823efb357c22762
lucaspere <lucasrupestre@hotmail.com>
2021-11-21 02:50:36 -0300

    atualiza metodos do Repositorio

--- Código/server/src/repository/EmpresaRepository.ts
+++ Código/server/src/repository/EmpresaRepository.ts
@@ -1,4 +1,6 @@
 import fsDB from '../db/fs-acess'
+import { MessageEntity } from '../interfaces/MessageStore';
+import Empresa from '../models/Empresa';
 
 class EmpresaRepository {
     async cadastrarVantagem(key: string, data: any) {
@@ -7,16 +9,25 @@ class EmpresaRepository {
         return result;
     }
 
-    async deletarVantagem(query: { cnpj: string; vid: string }, data: any) {
-        const result = fsDB.save(data);
+    async deletarVantagem(query: { cnpj: string; vid: string }) {
+        const result = fsDB.read(2 as any);
 
         return result;
     }
 
-    async alterarVantagem(key: any) {
-        const result = fsDB.save(key);
+    async alterarVantagem(query: { cnpj: string; vid: string }, data: any) {
+        const json = await fsDB.read(this.buildMessage(data))
+        const empresa = Empresa.fromJSON(json)
+        const result = fsDB.save(this.buildMessage(Empresa));
 
         return result;
+    }
+
+    private buildMessage(data: any) {
+        return {
+            modelName: 'empresa',
+            data
+        }
     }
 }
 
