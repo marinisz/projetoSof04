2753127c425b8ddd42cdd93d86a67bd1760f3aa5
lucaspere <lucasrupestre@hotmail.com>
2021-11-21 02:24:28 -0300

    cria FSStore

--- Código/server/src/db/fs-acess.ts
+++ Código/server/src/db/fs-acess.ts
@@ -1,10 +1,36 @@
 import fs from 'fs-extra';
 import path from 'path';
-import util from 'util';
 
-const filePath = process.env.PATH
-export default class FSStore {
-    save<T>(data: T) {
-        fs.writeFile
+import { Event } from '../repository/Model';
+
+class FSStore {
+    async save<T>(messag: Event<T>) {
+        const dir = await this.ensureDir();
+        const path = this.filePath(dir, messag.model);
+        const data = await fs.writeFile(path, messag.data);
+        return data;
+    }
+
+    async read<T>(messag: Event<T>) {
+        const dir = await this.ensureDir();
+        const path = this.filePath(dir, messag.model);
+        const data = await fs.readFile(path, 'utf8');
+
+        return data;
+    }
+
+    async ensureDir() {
+        const dir =
+            process.env.NOTES_FS_DIR || path.join(__dirname, 'fs-data');
+
+        await fs.ensureDir(dir);
+
+        return dir;
+    }
+
+    filePath(dir: string, key: string) {
+        return path.join(dir, `${key}.json`);
     }
 }
+
+export default new FSStore()
