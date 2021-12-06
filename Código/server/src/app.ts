17fd01334700b10b63ea2e856dd90f8410309d1a
lucaspere <lucasrupestre@hotmail.com>
2021-12-06 16:33:52 -0300

    adiciona novos recursos no app

--- Código/server/src/app.ts
+++ Código/server/src/app.ts
@@ -2,65 +2,64 @@ import express from 'express';
 import router from './routes';
 import prisma from './db';
 import cors from 'cors';
-
+import { default as DBG } from 'debug';
 import * as http from 'http';
-import { basicErrorHandler } from './appsupport';
+import { basicErrorHandler, onError, onListening } from './appsupport';
 
-const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 3000;
 const app = express();
+const debug = DBG('moedas:debug');
+var path = require('path');
+const HTML_DIR = path.join(__dirname, '/../../front');
 
-var path = require('path')
-const HTML_DIR = path.join(__dirname,'/../../front')
-app.use(express.static(HTML_DIR))
+app.use(express.static(HTML_DIR));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use('/api/', router);
 
 app.use(basicErrorHandler);
 
-const server = http.createServer(app);
+export const server = http.createServer(app);
 
-const boot = (): void => {
-    prisma.$connect().then(() => {
-        server.listen(PORT, () => {
-            console.log(`Server is up on port ${PORT}`);
+prisma
+    .$connect()
+    .then(() => {
+        server.listen(PORT);
+    })
+    .catch(error => {
+        onError({
+            code: 'EMOEDAPRISMA',
+            error,
         });
     });
-};
 
-const shutdown = () => {
-    prisma.$disconnect().then(() => {
-        server.close();
-    });
-};
+server.on('error', onError);
+server.on('listening', onListening);
+server.on('request', (req, res) => {
+    debug(`${new Date().toISOString()} request ${req.method} ${req.url}`);
+});
 
-if (require.main === module) boot();
-else {
-    console.info('Server running as a module');
-    module.exports = { boot, shutdown, PORT };
-}
+app.get('/alunos', function (req, res) {
+    res.sendFile(path.resolve(__dirname + '/../../front/view/aluno.html'));
+});
 
+app.get('/empresas', function (req, res) {
+    res.sendFile(path.resolve(__dirname + '/../../front/view/empresa.html'));
+});
 
-app.get('/alunos',function(req,res){
-    res.sendFile(path.resolve(__dirname+'/../../front/view/aluno.html'))
-})
+app.get('/moedas', function (req, res) {
+    res.sendFile(path.resolve(__dirname + '/../../front/view/moedas.html'));
+});
 
-app.get('/empresas',function(req,res){
-    res.sendFile(path.resolve(__dirname+'/../../front/view/empresa.html'))
-})
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname + '/../../front/view/index.html'));
+});
 
-app.get('/moedas',function(req,res){
-    res.sendFile(path.resolve(__dirname+'/../../front/view/moedas.html'))
-})
+app.get('/extrato', function (req, res) {
+    res.sendFile(path.resolve(__dirname + '/../../front/view/extrato.html'));
+});
 
-app.get('/',function(req,res){
-    res.sendFile(path.resolve(__dirname+'/../../front/view/index.html'))
-})
-
-app.get('/extrato',function(req,res){
-    res.sendFile(path.resolve(__dirname+'/../../front/view/extrato.html'))
-})
-
-app.get('/cadastrovantagem',function(req,res){
-    res.sendFile(path.resolve(__dirname+'/../../front/view/cadastrovantagem.html'))
-})
+app.get('/cadastrovantagem', function (req, res) {
+    res.sendFile(path.resolve(__dirname + '/../../front/view/cadastrovantagem.html'));
+});
--- Código/server/src/appsupport.ts
+++ Código/server/src/appsupport.ts
@@ -1,19 +1,114 @@
-import { ErrorRequestHandler } from "express";
-import util from 'util';
+import prisma from './db';
+import { server, PORT } from './app';
+import { default as DBG } from 'debug';
+import * as util from 'util';
+import { ErrorRequestHandler, RequestHandler } from 'express';
 
+const debug = DBG('moedas:debug');
+const dbgerror = DBG('moedas:error');
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+export const normalizePort = (val: string) => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+};
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+type Error = {
+    syscall?: string;
+    code: string;
+    error: Error;
+};
+export const onError = (error: Error) => {
+    dbgerror(error);
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof PORT === 'string' ? 'Pipe ' + PORT : 'Port ' + PORT;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+            break;
+        case 'EMOEDAPRISMA':
+            console.error(`Prisma store initialization failure because `, error.error);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+export const onListening = () => {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr?.port;
+    debug(`Listening on ${bind}`);
+};
+
+export const handle404: RequestHandler = (req, res, next) => {
+    const err: any = new Error('Not Found');
+    err.status = 404;
+    next(err);
+};
 
 export const basicErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     // Defer to built-in error handler if headersSent
     // See: http://expressjs.com/en/guide/error-handling.html
     if (res.headersSent) {
         console.log(`basicErrorHandler HEADERS SENT error ${util.inspect(err)}`);
-        return next(err)
+        return next(err);
     }
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+    dbgerror(`basicErrorHandler ${err.status || 500} ${util.inspect(res.locals)} ${util.inspect(err)}`);
+
     // render the error page
     res.status(err.status || 500);
-    res.send('error '+err);
+    res.render('error');
+};
+
+async function catchProcessDeath() {
+    debug('urk...');
+    await prisma.$disconnect();
+    await server.close();
+    process.exit(0);
 }
+
+process.on('SIGTERM', catchProcessDeath);
+process.on('SIGINT', catchProcessDeath);
+process.on('SIGHUP', catchProcessDeath);
+
+process.on('uncaughtException', function (err) {
+    console.error(`I've crashed!!! - ${err.stack || err}`);
+});
+process.on('unhandledRejection', (reason, p) => {
+    console.error(`Unhandled Rejection at: ${util.inspect(p)}\nreason: ${reason}`);
+});
