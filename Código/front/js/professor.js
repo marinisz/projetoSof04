146493d17b32cbeef8ed805ce93e9a854a191635
lucaspere <lucasrupestre@hotmail.com>
2021-12-06 16:33:20 -0300

    adiciona departamento nome

--- Código/front/js/professor.js
+++ Código/front/js/professor.js
@@ -56,7 +56,7 @@ async function buscaProfessores(){
                 let celula3 = novaLinha.insertCell(2);
                 celula3.innerHTML=professores[i].cpf
                 let celula4 = novaLinha.insertCell(3);
-                celula4.innerHTML=professores[i].departamento
+                celula4.innerHTML=professores[i].departamento.nome
                 let celula5 = novaLinha.insertCell(4);
                 celula5.innerHTML=`<svg onclick="retornaBuscado(${professores[i].id})" id="iconEditar" style="cursor:pointer" data-bs-toggle="modal" data-bs-target="#staticBackdrop2" id="icones" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16">
                 <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z"/>
